8319f79c040815cdaac65da534da0cdfba4aa8e0
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-01 13:52:44 +1000

    added error handling

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,11 +1,15 @@
 from app import app
+from flask import request
 
 
 @app.route("/")
 @app.route("/hello", methods=['GET', 'POST'])
 def hello():
-	if request.form["string"]:
-		string = request.from["string"]
-	else:
-		string = 'Hello, World!'
+	try:
+		if request.form["string"]:
+			string = request.from["string"]
+		else:
+			string = 'Hello, World!'
+	except:
+		abort(400)
 	return string
