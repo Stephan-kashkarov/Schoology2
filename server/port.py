c3819d833c71d1a63ee09db1883ba6b9c6281eba
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-01 14:53:46 +1000

    made the comandline work

--- server/port.py
+++ server/port.py
@@ -1,3 +1,14 @@
-from app import app
+from app import app, db
+from app.models import User, Post, Following
 
 print("-" * 25 + "\n Ports server is ONLINE \n" + "-" * 25)
+
+
+@app.shell_context_processor
+def make_shell_context():
+	return {
+		'db': db,
+		'User': User,
+		'Post': Post,
+		'Following': Following
+	}
